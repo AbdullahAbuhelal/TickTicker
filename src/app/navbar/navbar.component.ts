5ecc374911914bfd36b8484ce9d88229f19d75c5
AbdullahAbuhelal <abououd4@gmail.com>
2023-07-05 12:29:35 +0300

    api key as environment variable, search method works, add tailwind

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +11,33 @@ export class NavbarComponent {
 
   isDark = false;
   isDarkIconSrc = "assets/icons/icon _moon_.svg";
-  iconStyles = {'width.px':25, 'height.px':25}
+  iconStyles = {'width.px':25, 'height.px':25};
+  searchResult: any[] = [];
+
+  searchKeyword = "";
+
+  constructor(private http: HttpClient) {}
+
+  searchTickers(keyword: string) {
+    let searchUrl = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + keyword +"&apikey=" + environment.APIKEY;
+    if (keyword.length>0) {
+      this.http.get(searchUrl).subscribe(
+        (data) => {
+
+          this.searchResult = JSON.parse(JSON.stringify(data)).bestMatches;
+          console.log(this.searchResult);
+          console.log(this.searchResult[0]["2. name"]);
+
+        },
+        (error) => {
+          console.log('something went wrong')
+          console.log(error.toString());
+        }
+      );
+    } else {
+      this.searchResult = [];
+    }
+  }
   onDarkModeClicked() {
     document.body.classList.toggle('dark-mode');
     this.isDarkIconSrc = (this.isDark)? "assets/icons/icon _moon_.svg": "assets/icons/icon _sun 1_.svg";
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  APIKEY: "4RXAOYIJYK65H7UC"
+};
