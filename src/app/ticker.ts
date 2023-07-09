92600e146e5bd051e51be3d844bdc125461cfc02
AbdullahAbuhelal <abououd4@gmail.com>
2023-07-09 12:19:33 +0300

    ticker price interface added

---
+++ src/app/ticker.ts
@@ -0,0 +1,16 @@
+export interface tickerPriceTimeSeriesStamp {
+  "1. open": number,
+  "2. high": number,
+  "3. low": number,
+  "4. close": number,
+  "5. volume": number
+}
+
+export interface tickerMetaData {
+  "1. Information": string,
+  "2. Symbol": string,
+  "3. Last Refreshed": string,
+  "4. Interval": string,
+  "5. Output Size": string,
+  "6. Time Zone": string
+}
