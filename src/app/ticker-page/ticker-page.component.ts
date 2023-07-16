a3f7f4530a18b9bcd82f52ad830964f009ac6205
AbdullahAbuhelal <abououd4@gmail.com>
2023-07-16 13:17:11 +0300

    add charjs to the project, create a sample graph

--- src/app/ticker-page/ticker-page.component.ts
+++ src/app/ticker-page/ticker-page.component.ts
@@ -5,6 +5,7 @@ import {environment} from "../../environments/environment";
 import { tickerPriceTimeSeriesStamp, tickerQuoteEndpoint} from "../ticker";
 import {data} from "autoprefixer";
 import {firstValueFrom} from "rxjs";
+import {Chart} from "chart.js/auto";
 
 @Component({
   selector: 'app-ticker-page',
@@ -117,6 +118,8 @@ export class TickerPageComponent {
       this.saveTickerStyle();
     }
     console.log("saved list", this.savedTickersList, this.isTickerSaved, tickerFind);
+
+    this.createChart();
   }
 
   async onTickerTypeReceived(type: string) {
@@ -269,6 +272,37 @@ export class TickerPageComponent {
 
   onClick(newType: string) {
     this.currentChartType = newType
+  }
+
+  chart: any
+  createChart(){
+
+    this.chart = new Chart("FirstChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+          '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ],
+        datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+              '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+              '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
   }
 }
 
