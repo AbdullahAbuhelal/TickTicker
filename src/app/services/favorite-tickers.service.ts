fb76970ed24bc7eb534b6f25b7f68cae2e2b04b5
AbdullahAbuhelal <abououd4@gmail.com>
2023-07-27 13:58:48 +0300

    add: adding tickers and removing tickers to the service and link it with ticker page

--- src/app/services/favorite-tickers.service.ts
+++ src/app/services/favorite-tickers.service.ts
@@ -6,11 +6,33 @@ import { Injectable } from '@angular/core';
 export class FavoriteTickersService {
 
   FavoriteTickersKey = 'savedTickersList'
-  savedList = []
+  savedList: string[]
+
   getFavoriteTickers() {
-    if (this.savedList.length == 0) this.savedList = JSON.parse(`${localStorage.getItem(this.FavoriteTickersKey)}`) ?? []
     return this.savedList
   }
 
-  constructor() { }
+  isTickerSaved(ticker: string) {
+    let tickerFind = this.savedList.find((symbol) => symbol==ticker);
+    return typeof tickerFind != "undefined";
+  }
+
+  removeTicker(ticker: string) {
+    let tickerIndex = this.savedList.indexOf(ticker);
+    this.savedList.splice(tickerIndex, 1);
+    this.updateStorage()
+  }
+
+  addTicker(ticker: string) {
+    this.savedList.push(ticker)
+    this.updateStorage()
+  }
+
+  private updateStorage() {
+    localStorage.setItem("savedTickersList", JSON.stringify(this.savedList))
+  }
+
+  constructor() {
+    this.savedList = JSON.parse(`${localStorage.getItem(this.FavoriteTickersKey)}`) ?? []
+  }
 }
