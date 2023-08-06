c0db0830982d0ee66f1c4bbe10ff1f84320cb1d3
AbdullahAbuhelal <abououd4@gmail.com>
2023-08-06 15:17:57 +0300

    make the list of saved ticker dynamic so when one deleted from the home it updates

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -21,46 +21,64 @@ export class MainPageComponent {
   isLoadingTiles= true;
   isThereFavorites = false;
 
+  private loadedList: string[] = []
+
 
   ngOnInit() {
-    let savedTickersList = this.favoriteTickersService.getFavoriteTickers()
-    if (savedTickersList.length == 0) this.isLoadingTiles = false;
-    this.isThereFavorites = savedTickersList.length > 0;
-    // GET tickers quotes
-    savedTickersList.forEach((tickerSymbol: any)  => {
-      let quoteUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${tickerSymbol}&apikey=${environment.APIKEY}`
-      let tickerQuote: tickerQuoteEndpoint = {
-        "01. symbol": "",
-        "02. open": 0,
-        "03. high": 0,
-        "04. low": 0,
-        "05. price": 0,
-        "06. volume": 0,
-        "07. latest trading day": new Date(),
-        "08. previous close": 0,
-        "09. change": 0,
-        "10. change percent": 0
-      };
-      this.isLoadingTiles = true
-      this.http.get(quoteUrl).subscribe(
-        (data) => {
-          try {
-            tickerQuote = JSON.parse(JSON.stringify(data))["Global Quote"];
-            let tickerPrice = tickerQuote["05. price"];
-            this.savedTickers.push({symbol: tickerSymbol, price: tickerPrice})
-            this.isLoadingTiles = false
-          } catch (e) {
-            this.savedTickers.push({symbol: tickerSymbol, price: NaN})
-            this.isLoadingTiles = false
+    // let savedTickersList : any = []
+
+    this.favoriteTickersService.getFavoriteTickers().subscribe(
+      (savedTickersList) => {
+        this.savedTickers = []
+        if (savedTickersList.length == 0) this.isLoadingTiles = false;
+        this.isThereFavorites = savedTickersList.length > 0;
+        // GET tickers quotes
+        savedTickersList.forEach((tickerSymbol: any)  => {
+          let tickerIndex = this.loadedList.indexOf(tickerSymbol??"")
+          if (tickerIndex==(-1))  {
+
+            let quoteUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${tickerSymbol}&apikey=${environment.APIKEY}`
+            let tickerQuote: tickerQuoteEndpoint = {
+              "01. symbol": "",
+              "02. open": 0,
+              "03. high": 0,
+              "04. low": 0,
+              "05. price": 0,
+              "06. volume": 0,
+              "07. latest trading day": new Date(),
+              "08. previous close": 0,
+              "09. change": 0,
+              "10. change percent": 0
+            };
+            this.isLoadingTiles = true
+
+            this.http.get(quoteUrl).subscribe(
+              (data) => {
+                try {
+                  tickerQuote = JSON.parse(JSON.stringify(data))["Global Quote"];
+                  let tickerPrice = tickerQuote["05. price"];
+                  this.savedTickers.push({symbol: tickerSymbol, price: tickerPrice})
+                  this.isLoadingTiles = false
+                } catch (e) {
+                  this.savedTickers.push({symbol: tickerSymbol, price: NaN})
+                  this.isLoadingTiles = false
+                }
+              },
+              (error) => {
+                console.log('something went wrong')
+                console.log(error.toString());
+                this.savedTickers.push({symbol: tickerSymbol, price: NaN})
+              }
+            )
+
           }
-        },
-        (error) => {
-          console.log('something went wrong')
-          console.log(error.toString());
-          this.savedTickers.push({symbol: tickerSymbol, price: NaN})
-        }
-      )
-    })
+
+        })
+
+      }
+    )
+
+
     // this.isLoadingTiles = false;
 
   }
