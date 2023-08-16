5618041b5a93a3d89436a5f973cfb71a18ac58ac
AbdullahAbuhelal <abououd4@gmail.com>
2023-08-16 14:58:50 +0300

    stock api now can get stock summary and now main page is not responsible of getting the price of the saved stocks

--- src/app/Components/main-page/main-page.component.ts
+++ src/app/Components/main-page/main-page.component.ts
@@ -1,8 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {environment} from "../../../environments/environment";
-import {HttpClient} from "@angular/common/http";
-import {tickerQuoteEndpoint} from "../../ticker";
+import {Component} from '@angular/core';
 import {SavedTickersService} from "../../services/saved-tickers/saved-tickers.service";
+import {StockApiService} from "../../services/stock-api/stock-api.service";
 
 @Component({
   selector: 'app-main-page',
@@ -12,8 +10,8 @@ import {SavedTickersService} from "../../services/saved-tickers/saved-tickers.se
 export class MainPageComponent {
 
   constructor(
-    private http: HttpClient,
-    private favoriteTickersService: SavedTickersService
+    private favoriteTickersService: SavedTickersService,
+    private stockApiService: StockApiService
     ) {}
 
   savedTickers: {symbol: string, price: number}[] = []
@@ -21,65 +19,21 @@ export class MainPageComponent {
   isLoadingTiles= true;
   isThereFavorites = false;
 
-  private loadedList: string[] = []
-
-
   ngOnInit() {
-    // let savedTickersList : any = []
-
     this.favoriteTickersService.getFavoriteTickers().subscribe(
       (savedTickersList) => {
         this.savedTickers = []
-        if (savedTickersList.length == 0) this.isLoadingTiles = false;
+        this.isLoadingTiles = savedTickersList.length != 0;
         this.isThereFavorites = savedTickersList.length > 0;
-        // GET tickers quotes
-        savedTickersList.forEach((tickerSymbol: any)  => {
-          let tickerIndex = this.loadedList.indexOf(tickerSymbol??"")
-          if (tickerIndex==(-1))  {
-
-            let quoteUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${tickerSymbol}&apikey=${environment.APIKEY}`
-            let tickerQuote: tickerQuoteEndpoint = {
-              "01. symbol": "",
-              "02. open": 0,
-              "03. high": 0,
-              "04. low": 0,
-              "05. price": 0,
-              "06. volume": 0,
-              "07. latest trading day": new Date(),
-              "08. previous close": 0,
-              "09. change": 0,
-              "10. change percent": 0
-            };
-            this.isLoadingTiles = true
-
-            this.http.get(quoteUrl).subscribe(
-              (data) => {
-                try {
-                  tickerQuote = JSON.parse(JSON.stringify(data))["Global Quote"];
-                  let tickerPrice = tickerQuote["05. price"];
-                  this.savedTickers.push({symbol: tickerSymbol, price: tickerPrice})
-                  this.isLoadingTiles = false
-                } catch (e) {
-                  this.savedTickers.push({symbol: tickerSymbol, price: NaN})
-                  this.isLoadingTiles = false
-                }
-              },
-              (error) => {
-                console.log('something went wrong')
-                console.log(error.toString());
-                this.savedTickers.push({symbol: tickerSymbol, price: NaN})
-              }
-            )
-
-          }
-
+        savedTickersList.forEach(async (tickerSymbol: any)  => {
+          this.isLoadingTiles = true
+          this.savedTickers.push({
+            symbol: tickerSymbol,
+            price: await this.stockApiService.getStockPrice(tickerSymbol)
+          })
+          this.isLoadingTiles = false
         })
-
       }
     )
-
-
-    // this.isLoadingTiles = false;
-
   }
 }
--- src/app/services/stock-api/stock-api.service.ts
+++ src/app/services/stock-api/stock-api.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../../environments/environment";
-import {map, Observable} from "rxjs";
+import {firstValueFrom, map, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {tickerQuoteEndpoint} from "../../ticker";
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,42 @@ export class StockApiService {
     );
     else
       return new Observable<any>
+  }
+
+  async getStockSummary(ticker: string) {
+    let quoteUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${environment.APIKEY}`
+    let tickerQuote: tickerQuoteEndpoint = {
+      "01. symbol": "",
+      "02. open": NaN,
+      "03. high": NaN,
+      "04. low": NaN,
+      "05. price": NaN,
+      "06. volume": 0,
+      "07. latest trading day": new Date(),
+      "08. previous close": 0,
+      "09. change": 0,
+      "10. change percent": 0
+    };
+    let getObservable = this.http.get(quoteUrl)
+    let data = await firstValueFrom(getObservable)
+    try {
+      tickerQuote = JSON.parse(JSON.stringify(data))["Global Quote"];
+    } catch (e) {}
+
+    // subscribe(
+    //   (data) => {
+    //     try {
+    //       tickerQuote = JSON.parse(JSON.stringify(data))["Global Quote"];
+    //     } catch (e) {}
+    //   },
+    //   (error) => {}
+    // )
+    return tickerQuote
+  }
+
+  async getStockPrice(ticker: string) {
+    let summary = await this.getStockSummary(ticker)
+    return summary["05. price"] ?? NaN
   }
 
   constructor(
